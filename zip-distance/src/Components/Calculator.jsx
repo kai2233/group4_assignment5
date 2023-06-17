d23512464dbae612dc3d3362decfbf7026375296
Kaifeng <1536771545kai@gmail.com>
2023-06-16 20:39:40 -0400

    update

--- zip-distance/src/Components/Calculator.jsx
+++ zip-distance/src/Components/Calculator.jsx
@@ -4,30 +4,30 @@ const Calculator =()=> {
     const [distanceState, setdistanceState] = useState ([])
     const [input1,setInput1] = useState ("") 
     const [input2,setInput2] = useState ("") 
-    var distanceInMiles = "_____";
+    // var distanceInMiles = "_____";
     async function fetchInfo(){
         const result = await axios.get(`https://zip-api.eu/api/v1/distance/US-${input1}/US-${input2}/mi`) 
         console.log(result)
         setdistanceState(result.data)
-        distanceInMiles = Number(distanceState.distance).toFixed(2);
+        // distanceInMiles = Number(distanceState.distance).toFixed(2);
         
     }
 
-   
+    const distanceInMiles = Number(distanceState.distance).toFixed(2);
 
     console.log(distanceInMiles);
 return(
     
     <div>
-        <form onSubmit={fetchInfo}> 
+        {/* <form onSubmit={fetchInfo}>  */}
        <input type="text" placeholder="starting zip code" onChange={e=>{setInput1(e.target.value)}}/> 
        <br></br>
-       <input type="submit" placeholder="ending zip code" onChange={e=>{setInput2(e.target.value)}}/> 
+       <input type="text" placeholder="ending zip code" onChange={e=>{setInput2(e.target.value)}}/> 
        <br></br>
-       {/* <button type="submit"> Submit </button>  */}
-        {/* <h1>The distance between this two zip code is {distanceInMiles} miles </h1> */}
-        </form>
-        {distanceInMiles &&<h1>The distance between this two zip code is {distanceInMiles} miles </h1>}
+       <button onClick={fetchInfo}> Submit </button> 
+        <h1>The distance between this two zip code is {distanceInMiles} miles </h1>
+        {/* </form> */}
+        {/* {distanceInMiles &&<h1>The distance between this two zip code is {distanceInMiles} miles </h1>} */}
 
         
     </div>
